3481d41a80a7295d0b5c1ab25189058a613a3365
Zebdron <sebinalb@gmail.com>
2020-08-19 15:25:39 +0530

    Implemented Callbacks & Error Handling

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 var rect = require('./rect');
 
-function solve(l,b){
-    console.log("Solving Peri & Area");
-    if(l<=0 || b<=0){
-        console.log("Not Solving Peri & Area");
-    }
-    else{
-        console.log("Solving Peri "+ rect.perimeter(l,b));
-        console.log("Solving  Area" + rect.area(l,b));
-    }
-}
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
 
-solve(2,3);
-solve(0,2);
+solveRect(2,3);
+solveRect(3,5);
+solveRect(0,2);
